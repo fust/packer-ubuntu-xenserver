41db81f0290cf541efc2f28fa34dc5c250e0e475
Thom Wijtenburg <thom@thomwijtenburg.nl>
2023-06-07 12:43:33 +0200

    Remove Ubuntu 18, add Ubuntu 20

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -9,8 +9,16 @@ apt autoremove
 apt update
 
 # Delete unneeded files.
-rm -f /home/ansible/*.sh
+rm -f /home/vagrant/*.sh
 
+# Disable password login for Ansible user
+passwd vagrant -l
+
+# Disable root login with password
+sed -i 's/PermitRootLogin no//g' /etc/ssh/sshd_config
+echo  -e 'PermitRootLogin without-password' >> /etc/ssh/sshd_config
+
+echo 'Zeroing free space'
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
